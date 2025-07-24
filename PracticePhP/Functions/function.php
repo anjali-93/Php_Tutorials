1a041176a784ecf870686d8693eaa1c7ae51d683
Anjali Saxena <anjalisaxena667@gmail.com>
2025-07-24 13:10:29 +0530

    yesterday work

---
+++ PracticePhP/Functions/function.php
@@ -0,0 +1,32 @@
+<?php
+
+function test(){
+    echo "<h1>User Details :</h1>";
+}
+
+function DisplayUserDetail()
+{
+    test();
+    echo "UserName : Tommy Cruise <br>";
+    echo "UserAge : 45 <br>";
+    echo "UserEmail : tommy@gmail.com <br>";
+    echo "<hr>";
+}
+
+DisplayUserDetail();
+DisplayUserDetail();
+DisplayUserDetail();
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
