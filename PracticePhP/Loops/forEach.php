b51fad9e34b9c3c1debfa237a7ef975ddb9d34eb
Anjali Saxena <anjalisaxena667@gmail.com>
2025-08-12 19:04:37 +0530

    forEach loop

---
+++ PracticePhP/Loops/forEach.php
@@ -0,0 +1,25 @@
+<?php
+
+$users = ["Alice", "Bob", "Charlie", "David", "Eve" ,"Frank", "Grace", "Heidi", "Ivan", "Judy"];
+
+// foreach ($users as $x){
+   
+//     // echo "<br>";/
+//     if ($x == "David"){
+//         continue; 
+//     }
+//      echo "<h3> User: " . $x . "</h3>";
+//     // if($x == "David"){
+//     //     break;
+//     // }
+// }
+
+foreach ($users as $x):
+    echo $x;
+     echo "<br>";
+     endforeach
+
+
+
+
+?>
